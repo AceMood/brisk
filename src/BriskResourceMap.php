a19cd0a8d29817b5047fa88a1fbda2c8d63b7a4c
AceMood <zmike86@163.com>
2016-08-22 19:35:23 +0800

    ordered packages resources

--- src/BriskResourceMap.php
+++ src/BriskResourceMap.php
@@ -229,7 +229,7 @@ final class BriskResourceMap extends Phobject {
         return $map;
     }
 
-    //给一个资源名,查询所有依赖并存入一个map结构
+    //给一个资源名, 查询所有递归的依赖, 并存入一个map结构
     private function resolveResource(array &$map, $name) {
         if (!isset($this->nameMap[$name])) {
             throw new Exception(pht(
@@ -238,12 +238,13 @@ final class BriskResourceMap extends Phobject {
             ));
         }
 
-        // array(deps => array(), css => array());
+        // array(js => array(), css => array());
         $requires = $this->getRequiredSymbolsForName($name);
 
         $map[$name] = array();
         foreach ($requires as $type => $required_symbol) {
             $required_name = $this->getResourceNameForSymbol($type, $required_symbol);
+            //map中记录依赖项
             $map[$name][] = $required_name;
             if (isset($map[$required_name])) {
                 continue;
@@ -254,25 +255,25 @@ final class BriskResourceMap extends Phobject {
 
     // include all resources and packages they live in
     private function packageResources(array $resolved_map, $entry) {
+        //记录需要引入的有序资源列表
         $packaged = array();
+        //记录处理过的资源
         $handled = array();
-
-        $this->loop($packaged, $handled, $entry);
-        foreach ($resolved_map as $name => $requires) {
-            if (isset($handled[$name])) {
-                continue;
-            }
-            $this->loop($packaged, $handled, $name);
-        }
-
+        $this->loop($resolved_map, $packaged, $handled, $entry);
         return $packaged;
     }
 
-    private function loop(&$packaged, &$handled, $name) {
+    private function loop(&$resolved_map, &$packaged, &$handled, $name) {
         $symbol = $this->nameMap[$name];
         //并未打包
         if (empty($this->componentMap[$symbol])) {
             $packaged[] = $name;
+            foreach ($resolved_map[$name] as $require) {
+                if (isset($handled[$require])) {
+                    continue;
+                }
+                $this->loop($resolved_map, $packaged, $handled, $require);
+            }
         } else {
             $package_name = $this->componentMap[$symbol];
             $packaged[] = $package_name;
@@ -281,6 +282,12 @@ final class BriskResourceMap extends Phobject {
             foreach ($package_symbols as $resource_symbol) {
                 $resource_name = $this->getResourceNameForSymbol($package_type, $resource_symbol);
                 $handled[$resource_name] = true;
+                foreach ($resolved_map[$resource_name] as $require) {
+                    if (isset($handled[$require])) {
+                        continue;
+                    }
+                    $this->loop($resolved_map, $packaged, $handled, $require);
+                }
             }
         }
     }
