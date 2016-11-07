31ea56e3ae664545a4edf563ee8638ada5793abe
AceMood <zmike86@163.com>
2016-11-07 21:08:51 +0800

    dev

--- src/const.php
+++ src/const.php
@@ -15,7 +15,17 @@ define('DEVICE_PC', 9800000);
 define('DEVICE_MOBILE', 9800001);
 
 // 打印资源表的输出类型.
-//
 define('MAP_NO', 9800002);
 define('MAP_ASYNC', 9800003);
 define('MAP_ALL', 9800004);
+
+// 页面的三种渲染模式, 对于bigpipe目前依赖于服务端实现.
+// 在渲染曾里面不会涉及真正并行的数据获取, 而是通过pagelet的优先级优先刷新缓存.
+// ajaxpipe和normal是通过get参数加以区分的. 默认页面渲染模式是normal.
+define('RENDER_NORMAL', 9800005);
+define('RENDER_AJAXPIPE', 9800006);
+define('RENDER_BIGPIPE', 9800007);
+
+// 组件的三种渲染模式, normal, bigrender和lazyrender
+define('RENDER_BIGRENDER', 9800009);
+define('RENDER_LAZYRENDER', 9800010);
