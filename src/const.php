529cc4e957e4ea8d4080214d158b8a99d2e71ad8
AceMood <zmike86@163.com>
2016-11-07 16:09:44 +0800

    add const file

--- src/const.php
+++ src/const.php
@@ -1,7 +1,21 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: baidu
- * Date: 16/11/7
- * Time: 下午3:25
+ * @file 定义常用常量
+ * @author AceMood
+ * @email zmike86@gmail.com
  */
+
+//---------------
+
+// 浏览网页的设备类型, 针对pc端用户, 分析依赖的静态资源全部外链输出.
+// 针对移动端用户, 目前采用inline的方式输出静态资源, 未来http2得到普及
+// 则可以修改为外链
+define('DEVICE_PC', 9800000);
+define('DEVICE_MOBILE', 9800001);
+
+// 打印资源表的输出类型.
+//
+define('MAP_NO', 9800002);
+define('MAP_ASYNC', 9800003);
+define('MAP_ALL', 9800004);
