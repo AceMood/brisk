ead393677d05e3097548c8a3e78679c877653a4a
AceMood <zmike86@163.com>
2016-11-07 17:26:37 +0800

    rename  to

--- src/api.php
+++ src/api.php
@@ -1,53 +1,52 @@
 <?php
 
-require_once '__init__.php';
-
 /**
- * Include a CSS or JS static resource by name. This function records a
- * dependency for the current page, so when a response is generated it can be
- * included. You can call this method from any context, and it is recommended
- * you invoke it as close to the actual dependency as possible so that page
- * dependencies are minimized.
- *
+ * @file 提供全局的应用程序接口
+ * @author AceMood
+ * @email zmike86@gmail.com
+ */
+
+//-------------
+
+// 最重要的api. 根据资源名称引用需要的CSS或JS. 这个接口记录当前页面需要的资源依赖,
+// 当返回请求时, 所有被以来的资源都会计算输出. 可以在任何地方调用这个函数, 一般推荐
+// 在组件内部`require_static`该组件依赖的文件, 这样保证组件是内聚的, 容易扩展和维护.
+/**
  * @param string $name 资源路径名
  * @param string $source_name 项目命名空间
  * @return void
  */
-function require_static_resource($name, $source_name) {
-    $response = BriskAPI::getStaticResourceResponse();
-    $response->requireResource($name, $source_name);
+function require_static($name, $source_name) {
+  $response = BriskAPI::staticResourceResponse();
+  $response->requireResource($name, $source_name);
 }
 
-//将一个资源数据内联式立即输出
-function inline_static_resource($name, $source_name) {
-    $response = BriskAPI::getStaticResourceResponse();
-    return $response->inlineResource($name, $source_name);
+/**
+ * @param string $name 资源路径名
+ * @param string $source_name 项目命名空间
+ * @return string 将一个资源数据内联式立即输出
+ */
+function inline_static($name, $source_name) {
+  $response = BriskAPI::staticResourceResponse();
+  return $response->inlineResource($name, $source_name);
 }
 
-//动态设置cdn
-function set_cdn($cdn) {
-    $response = BriskAPI::getStaticResourceResponse();
-    $response->setCDN($cdn);
-}
-
-//获取cdn
-function get_cdn() {
-    $response = BriskAPI::getStaticResourceResponse();
-    $response->getCDN();
-}
-
-//输出所有外链css
+/**
+ * 输出所有外链css
+ */
 function render_css_block() {
-    $response = BriskAPI::getStaticResourceResponse();
-    $content = $response->renderResourcesOfType('css');
-    echo $content->getHTMLContent();
+  $response = BriskAPI::staticResourceResponse();
+  $content = $response->renderResourcesOfType('css');
+  echo $content->getHTMLContent();
 }
 
-//输出所有外链js
+/**
+ * 输出所有外链js
+ */
 function render_js_block() {
-    $response = BriskAPI::getStaticResourceResponse();
-    $content = $response->renderResourcesOfType('js');
-    echo $content->getHTMLContent();
+  $response = BriskAPI::staticResourceResponse();
+  $content = $response->renderResourcesOfType('js');
+  echo $content->getHTMLContent();
 }
 
 /**
@@ -56,8 +55,8 @@ function render_js_block() {
  * @return  string  Versioned path to the image, if one is available.
  */
 function get_resource_uri($resource, $source_name = 'brisk') {
-    $resource = ltrim($resource, '/');
-    $map = BriskResourceMap::getNamedInstance($source_name);
-    $response = BriskAPI::getStaticResourceResponse();
-    return $response->getURI($map, $resource);
+  $resource = ltrim($resource, '/');
+  $map = BriskResourceMap::getNamedInstance($source_name);
+  $response = BriskAPI::staticResourceResponse();
+  return $response->getURI($map, $resource);
 }
