0bf2ead0e32d7aed9e3be4018ca0a7f062e47649
AceMood <zmike86@163.com>
2016-11-08 22:07:27 +0800

    add

--- src/page/BriskPageletInterface.php
+++ src/page/BriskPageletInterface.php
@@ -7,14 +7,36 @@
 
 interface BriskPageletInterface {
 
+  // 设置分片的渲染模式
   public function setMode($mode);
 
+  // 获取分片的渲染模式
   public function getMode();
 
-  public function render();
+  // 设置分片id
+  public function setId($id);
 
-  public function setDataSource();
+  // 获取分片id
+  public function getId();
+
+  // 设置分片的dom属性
+  public function setDomAttributes($attributes);
+
+  // 获取分片的dom属性
+  public function getDomAttributes();
+
+  // 渲染当前页面分片
+  public function produceHTML();
+
+  //
+  public function requireResource($name, $source_name);
+
+  //
+  public function setDataSource($data);
 
   public function getDataSource();
+
+  //
+  public function render();
 
 }
