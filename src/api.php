b3481130f1f58c2f6435ee9d1db0baa2fb3a7bf9
AceMood <zmike86@163.com>
2016-11-08 19:37:09 +0800

    add

--- src/api.php
+++ src/api.php
@@ -79,38 +79,30 @@ function pht($text, $variant = null /* , ... */) {
 */
 }
 
+// 同一函数. 返回参数的值.
+// 有一些php语法会导致错误, 如下:
+//   new Thing()->doStuff();
+// 但是这样是可以的:
+//   id(new Thing())->doStuff();
+// 本函数就为此目的
 /**
- * Identity function, returns its argument unmodified.
- *
- * This is useful almost exclusively as a workaround to an oddity in the PHP
- * grammar -- this is a syntax error:
- *
- *    COUNTEREXAMPLE
- *    new Thing()->doStuff();
- *
- * ...but this works fine:
- *
- *    id(new Thing())->doStuff();
- *
- * @param   wild Anything.
- * @return  wild Unmodified argument.
+ * @param  wild $x 任何参数.
+ * @return wild 无任何改变的形参.
  */
 function id($x) {
   return $x;
 }
-
 
 /**
  * Access an array index, retrieving the value stored there if it exists or
  * a default if it does not. This function allows you to concisely access an
  * index which may or may not exist without raising a warning.
  *
- * @param   array   Array to access.
- * @param   scalar  Index to access in the array.
- * @param   wild    Default value to return if the key is not present in the
- *                  array.
- * @return  wild    If `$array[$key]` exists, that value is returned. If not,
- *                  $default is returned without raising a warning.
+ * @param  array  $array 取值的数组.
+ * @param  scalar $key 取值的索引.
+ * @param  wild   $default 没有找打返回此默认值
+ * @return wild   If `$array[$key]` exists, that value is returned. If not,
+ *                $default is returned without raising a warning.
  */
 function idx(array $array, $key, $default = null) {
   // isset() is a micro-optimization - it is fast but fails for null values.
