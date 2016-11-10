be94b7284da6afb824069e286dee6bf90b6f4258
AceMood <zmike86@163.com>
2016-11-10 19:24:31 +0800

    web page abstraction

--- src/resources/BriskResources.php
+++ src/resources/BriskResources.php
@@ -1,13 +1,14 @@
 <?php
 
 /**
- * @file Defines the location of static resources.
+ * @file 定义SR的基类.
  * @author AceMood
  * @email zmike86@gmail.com
  */
 
+//---------------
+
 abstract class BriskResources {
-  //文件后缀类型映射
   private static $typeMap = array(
     'jsx' => 'js',
     'js' => 'js',
@@ -21,32 +22,46 @@ abstract class BriskResources {
     'jpeg' => 'img',
     'webp' => 'img',
     'bmp' => 'img',
+    // Windows和Mac系统最常用的字体格式, 它们比基于矢量的字体更容易处理,
+    // 保证了屏幕与打印输出的一致性. 同时这类字体和矢量字体一样可以随意缩放,
+    // 旋转而不必担心会出现锯齿
+    'ttf' => 'font',
+    // 嵌入字体格式EOT是微软开发的一种技术, 允许OpenType字体嵌入到网页并
+    // 可以下载至浏览器渲染. 浏览器根据CSS中@font-face 的定义,下载渲染这种字体文件.
+    // 这些文件只在当前页活动的状态下, 临时安装在用户的系统中
+    'eot' => 'font',
+
+    'otf' => 'font',
+
+    'woff' => 'font',
+
+    'svg' => 'font'
   );
 
-  //项目名称作为命名空间
+  // 项目名称作为命名空间
   abstract public function getName();
 
-  //获取文件内容
+  // 获取文件内容
   abstract public function getResourceData($name);
 
-  //获取mtime
-  public function getResourceModifiedTime($name) {
+  // 获取文件mtime
+  function getResourceModifiedTime($name) {
     return 0;
   }
 
-  //获取资源类型 如js,css,img
-  public function getResourceType($path) {
+  // 获取资源类型 如js,css,img
+  function getResourceType($path) {
     $suffix = end(explode('.', $path));
     return self::$typeMap[$suffix];
   }
 
-  //加载资源表
-  public function loadMap() {
+  // 加载资源表
+  function loadMap() {
     return array();
   }
 
-  //加载打包信息
-  public function loadPackages() {
+  // 加载打包信息
+  function loadPackages() {
     return array();
   }
 }
--- src/resources/BriskResourcesOnDisk.php
+++ src/resources/BriskResourcesOnDisk.php
@@ -1,76 +1,71 @@
 <?php
 
 /**
- * Defines the location of physical static resources which exist at build time
+ * @file Defines the location of physical static resources which exist at build time
  * and are precomputed into a resource map.
+ * @author AceMood
+ * @email zmike86@gmail.com
  */
+
+//-------------
+
 abstract class BriskResourcesOnDisk extends BriskResources {
+  // resource.json转换来的资源表
+  private $map;
 
-    //resource.json转换来的资源表
-    private $map;
-    private $packages;
+  // packages.json转换来的资源表
+  private $packages;
 
-    //获取resource.json所在位置
-    abstract public function getPathToMap();
+  // 获取resource.json所在位置
+  abstract public function getPathToMap();
 
-    //获取packages.json位置
-    abstract public function getPathToPackageMap();
+  // 获取packages.json位置
+  abstract public function getPathToPackageMap();
 
-    // return source code directory
-    abstract public function getPathToResources();
+  // return source code directory
+  abstract public function getPathToResources();
 
-    // according to name as 'static/a.js', get the real file-system path
-    private function getPathToResource($name) {
-        $symbolMap = idx($this->map, 'resource', array());
-        $nameMap = idx($this->map, 'paths', array());
-        $symbol = $nameMap[$name];
-        $type = $this->getResourceType($name);
-        // todo 这里假设没有通过soi指定cdn, 或者是通过pageview设置的cdn, 此时uri是编译目录的相对路径
-        $uri = preg_replace('/^\//', '', $symbolMap[$type][$symbol]['uri']);
+  // according to name as 'static/a.js', get the real file-system path
+  private function getPathToResource($name) {
+    $symbolMap = idx($this->map, 'resource', array());
+    $nameMap = idx($this->map, 'paths', array());
+    $symbol = $nameMap[$name];
+    $type = $this->getResourceType($name);
+    // todo 这里假设没有通过编译工具指定cdn, 或者是通过pageview设置的cdn, 此时uri是编译目录的相对路径
+    $uri = preg_replace('/^\//', '', $symbolMap[$type][$symbol]['uri']);
 
-        return $this->getPathToResources() . DIRECTORY_SEPARATOR . $uri;
+    return $this->getPathToResources() . DIRECTORY_SEPARATOR . $uri;
+  }
+
+  /**
+   * 读取文件内容
+   * @param $name
+   * @return string
+   * @throws FilesystemException
+   */
+  public function getResourceData($name) {
+    return BriskFilesystem::readFile($this->getPathToResource($name));
+  }
+
+  function getResourceModifiedTime($name) {
+    return (int)filemtime($this->getPathToResource($name));
+  }
+
+  function loadMap() {
+    if ($this->map === null) {
+      $mapPath = $this->getPathToMap();
+      $data = BriskFilesystem::readFile($mapPath);
+      $this->map = json_decode($data, true);
     }
+    return $this->map;
+  }
 
-    /**
-     * 读取文件内容
-     * @param $name
-     * @return string
-     * @throws FilesystemException
-     */
-    public function getResourceData($name) {
-        return Filesystem::readFile($this->getPathToResource($name));
+  function loadPackages() {
+    if ($this->packages === null) {
+      $mapPath = $this->getPathToPackageMap();
+      $data = BriskFilesystem::readFile($mapPath);
+      $this->packages = json_decode($data, true);
     }
-
-    //获取资源mtime
-    public function getResourceModifiedTime($name) {
-        return (int)filemtime($this->getPathToResource($name));
-    }
-
-    /**
-     * 加载resource.json并转化成php数组
-     * @return mixed
-     * @throws FilesystemException
-     */
-    public function loadMap() {
-        if ($this->map === null) {
-            $mapPath = $this->getPathToMap();
-            $data = Filesystem::readFile($mapPath);
-            $this->map = json_decode($data, true);
-        }
-        return $this->map;
-    }
-
-    /**
-     * 加载packages.json并转化成php数组
-     * @return mixed
-     * @throws FilesystemException
-     */
-    public function loadPackages() {
-        if ($this->packages === null) {
-            $mapPath = $this->getPathToPackageMap();
-            $data = Filesystem::readFile($mapPath);
-            $this->packages = json_decode($data, true);
-        }
-        return $this->packages;
-    }
+    return $this->packages;
+  }
 }
